58d7c12f360e84d699f7e9249c380d84ec7f7668
Sergey Khristenko <sergey.khristenko@gmail.com>
2023-05-12 21:49:44 +0300

    wip

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,8 +1,10 @@
 cmdid=$(aws ssm send-command \
     --document-name "AWS-RunShellScript" \
     --targets "Key=InstanceIds,Values=i-061f5586b6765f895" \
-    --cli-input-json file://./deploy.json \
+    --cli-input-json file://./cicd/deploy.json \
     --query "Command.CommandId" \
     --output text)
 
-# aws ssm list-command-invocations --command-id "b82dc2cb-03e0-498f-bb96-1c857cee083b" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text
+echo "$cmdid"
+
+aws ssm list-command-invocations --command-id "634cdd78-82ce-4c25-9a93-d4cbac6904f8" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text
