2b885b6c60d56b79edceade4820aace0c44ae3e6
Sergey Khristenko <sergey.khristenko@gmail.com>
2023-05-12 19:48:36 +0300

    update deps (#6)

--- backend/deps.ts
+++ backend/deps.ts
@@ -1,4 +1,4 @@
-export { Application, Router, Status } from "https://deno.land/x/oak/mod.ts";
-export { crypto } from "https://deno.land/std/crypto/mod.ts";
+export { Application, Router, Status } from "https://deno.land/x/oak@v12.4.0/mod.ts";
+export { crypto } from "https://deno.land/std@0.186.0/crypto/mod.ts";
 export { ServerSentEvent } from "https://deno.land/x/oak@v12.4.0/deps.ts";
 export type { ServerSentEventTarget } from "https://deno.land/x/oak@v12.4.0/deps.ts";
