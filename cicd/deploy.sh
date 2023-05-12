220482d773de4d2c7bf70e39155796c203e522be
Sergey Khristenko <sergey.khristenko@gmail.com>
2023-05-12 22:40:58 +0300

    another one

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -1,10 +1,10 @@
 cmdid=$(aws ssm send-command \
     --document-name "AWS-RunShellScript" \
     --targets "Key=InstanceIds,Values=i-061f5586b6765f895" \
-    --cli-input-json file://./cicd/deploy.json \
+    --parameters 'commands=["/bin/sh /home/ubuntu/chatapp/deploy.sh"]' \
     --query "Command.CommandId" \
     --output text)
 
 echo "$cmdid"
 
-aws ssm list-command-invocations --command-id "634cdd78-82ce-4c25-9a93-d4cbac6904f8" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text
+# aws ssm list-command-invocations --command-id "5f4aac96-7abd-43cb-8e45-2b0f6dc2f6af" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text
