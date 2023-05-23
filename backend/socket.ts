dc9e883d25c7b1d7ea2e6952aa891eeab8ba14e7
Sergey Khristenko <sergey.khristenko@gmail.com>
2023-05-23 11:12:35 +0300

    update socket

--- backend/socket.ts
+++ backend/socket.ts
@@ -2,13 +2,13 @@ import { Server } from "socket.io";
 
 const devOptions = {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
   allowEIO3: true,
 };
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== "production";
 
 function setupSocketIO(httpServer) {
   const io = new Server(httpServer, isDev ? devOptions : {});
@@ -18,20 +18,28 @@ function setupSocketIO(httpServer) {
       socket.join(roomId);
       socket.broadcast.to(roomId).emit("user-connected", userId);
 
-      socket.on("new-offer", (offerObj) => {
-        socket.broadcast.to(roomId).emit("offer", offerObj);
+      socket.on("new-offer", (offerObj, userIdTo, userIdFrom) => {
+        socket.broadcast
+          .to(roomId)
+          .emit("offer", offerObj, userIdTo, userIdFrom);
       });
 
-      socket.on("new-answer", (answerObj) => {
-        socket.broadcast.to(roomId).emit("answer", answerObj);
+      socket.on("new-answer", (answerObj, userIdTo, userIdFrom) => {
+        socket.broadcast
+          .to(roomId)
+          .emit("answer", answerObj, userIdTo, userIdFrom);
       });
 
-      socket.on("new-offerCandidate", (iceCandidate) => {
-        socket.broadcast.to(roomId).emit("offerCandidate", iceCandidate);
+      socket.on("new-offerCandidate", (iceCandidate, userIdTo, userIdFrom) => {
+        socket.broadcast
+          .to(roomId)
+          .emit("offerCandidate", iceCandidate, userIdTo, userIdFrom);
       });
 
-      socket.on("new-answerCandidate", (iceCandidate) => {
-        socket.broadcast.to(roomId).emit("answerCandidate", iceCandidate);
+      socket.on("new-answerCandidate", (iceCandidate, userIdTo, userIdFrom) => {
+        socket.broadcast
+          .to(roomId)
+          .emit("answerCandidate", iceCandidate, userIdTo, userIdFrom);
       });
 
       socket.on("disconnect", () => {
